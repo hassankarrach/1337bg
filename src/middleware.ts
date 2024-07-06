b3dc1d70b213bc12056c6a348b7d9b82b498c72c
Zero <hassan.winners@gmail.com>
2024-07-06 21:29:16 +0100

    dbg middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
+  console.log('NODE_ENV:', process.env.NODE_ENV);
+  console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
+
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET as string,
-    salt: "next-auth.session-token",
+    salt: process.env.NODE_ENV === "production"
+      ? "__Secure-authjs.session-token"
+      : "authjs.session-token",
   });
-  
+
+  console.log('Token:', token);
+
   if (!token) {
     const url = req.nextUrl.clone();
     url.pathname = '/';
