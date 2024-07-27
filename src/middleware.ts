c238507b0e341ee485af0fe96af1b48e9fb16959
Zero <hassan.winners@gmail.com>
2024-07-28 00:20:01 +0100

    ▲ initing prisma

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET as string,
