6d1801ae88144b8205cf273c5f944e4fd9a1d718
Zero <hassan.winners@gmail.com>
2024-08-02 17:33:21 +0100

    weekly top performers

--- actions/user_action.ts
+++ actions/user_action.ts
@@ -1,17 +1,17 @@
-"use server";
+// "use server";
 
-import { prisma } from "@/utils/prisma";
+// import { prisma } from "@/utils/prisma";
 
-export async function create_user() {
-  const user = await prisma.user.create({
-    data: {
-      curr_level: 1,
-      last_level: 2,
-      user_name: "hkarrach",
-      created_at: new Date(),
-      is_verified: true,
-    },
-  });
+// export async function create_user() {
+//   const user = await prisma.user.create({
+//     data: {
+//       curr_level: 1,
+//       last_level: 2,
+//       user_name: "hkarrach",
+//       created_at: new Date(),
+//       is_verified: true,
+//     },
+//   });
 
-  console.log(user);
-}
+//   console.log(user);
+// }
