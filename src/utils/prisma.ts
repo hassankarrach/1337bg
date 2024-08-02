3c70541ae8e39a0620c1cd877bbf1aaf759b48d7
Zero <hassan.winners@gmail.com>
2024-08-02 17:32:04 +0100

    weekly top performers

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const GlobalPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// const GlobalPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
 
-export const prisma = GlobalPrisma.prisma ?? new PrismaClient();
+// export const prisma = GlobalPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  GlobalPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== "production") {
+//   GlobalPrisma.prisma = prisma;
+// }
