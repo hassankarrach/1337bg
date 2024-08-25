7ba4a9baa3d41b966b097ed42ffcf51666a31913
Zero <hassan.winners@gmail.com>
2024-08-25 14:04:50 +0100

    i'll broke the production

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,56 +1,56 @@
-"use client";
-import { useSession } from "next-auth/react";
-import React, { useState } from "react";
-import { toast } from "react-toastify";
-import styled from "styled-components";
+// "use client";
+// import { useSession } from "next-auth/react";
+// import React, { useState } from "react";
+// import { toast } from "react-toastify";
+// import styled from "styled-components";
 
-// import { create_user } from "../../../actions/user_action";
+// // import { create_user } from "../../../actions/user_action";
 
-const StyledTest = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  button {
-    padding: 5px 10px;
-    cursor: pointer;
-  }
-`;
+// const StyledTest = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   button {
+//     padding: 5px 10px;
+//     cursor: pointer;
+//   }
+// `;
 
-const page = () => {
-  // create user
-  const { data: session } = useSession();
+// const page = () => {
+//   // create user
+//   const { data: session } = useSession();
 
-  //hit post request to create user /api/students/create
-  const fetchData = async () => {
-    if (session) {
-      try {
-        //
-        const res = await fetch(
-          "https://api.intra.42.fr/v2/cursus_users?&filter[campus_id]=75&filter[begin_at]=2024-08-26T09:37:00.000Z&page[size]=100&page[number]=1&sort=-level",
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${session?.accessToken}`,
-            },
-          }
-        );
-        const data = await res.json();
-        console.log(data);
-      } catch (error) {
-        toast.error("Error creating user");
-      }
-    } else {
-      toast.error("Please login first");
-    }
-  };
+//   //hit post request to create user /api/students/create
+//   const fetchData = async () => {
+//     if (session) {
+//       try {
+//         //
+//         const res = await fetch(
+//           "https://api.intra.42.fr/v2/cursus_users?&filter[campus_id]=75&filter[begin_at]=2024-08-26T09:37:00.000Z&page[size]=100&page[number]=1&sort=-level",
+//           {
+//             method: "GET",
+//             headers: {
+//               Authorization: `Bearer ${session?.accessToken}`,
+//             },
+//           }
+//         );
+//         const data = await res.json();
+//         console.log(data);
+//       } catch (error) {
+//         toast.error("Error creating user");
+//       }
+//     } else {
+//       toast.error("Please login first");
+//     }
+//   };
 
-  return (
-    <StyledTest>
-      <button onClick={fetchData}>Create Me</button>
-    </StyledTest>
-  );
-};
+//   return (
+//     <StyledTest>
+//       <button onClick={fetchData}>Create Me</button>
+//     </StyledTest>
+//   );
+// };
 
-export default page;
+// export default page;
