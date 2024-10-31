9e364dcfbe5fbb4fb3ab54f6c33d3dd9074716f5
hassan karrach <zero@Zero.local>
2024-10-31 12:41:06 +0100

    test

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,14 @@
+// lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
-
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!globalThis.prisma) {
-    globalThis.prisma = new PrismaClient();
-  }
-  prisma = globalThis.prisma;
-}
+export const db = globalThis.prisma || new PrismaClient({
+  log: ['error', 'warn']
+});
 
-export const db = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = db;
+}
